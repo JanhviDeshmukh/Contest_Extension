4e593953479172d88ff2f64e4b76ccc6ccf49b2f
ramanshu-1216 <gawanderamanshu@gmail.com>
2022-07-03 02:35:16 +0530

    Updated

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const mongoose = require('mongoose');
 const { application } = require("express");
 mongoose.connect("mongodb+srv://admin:zxtqqD5rDBDkrjdT@cluster0.qquu6.mongodb.net/contestDB", { useNewUrlParser: true });
 
-const app = express();
-// setInterval(scrape, 1000 * 60 * 60);
+// const app = express();
+setInterval(scrape, 1000 * 60 * 60);
 
 scrape();
 
